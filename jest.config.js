b85033fb68b3d3009742c8c3d73b541038bf1bf0
Adam Butterfield <adam.butterfield@ymail.com>
2023-07-07 15:15:29 +0900

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
+  testRegex: '.+.test.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['src/*.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/types.d.ts'],
+};
