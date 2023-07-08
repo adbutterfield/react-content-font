271418fc9ccde5ed3706ab9171a498f5539dd85a
Adam Butterfield <adam.butterfield@ymail.com>
2023-07-08 23:01:43 +0900

    Consolidate eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,36 @@
+/** @type {import('eslint').Linter.Config} */
+
 module.exports = {
+  ignorePatterns: '.eslintrc.js',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2015,
-    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.jest.json'],
+    tsconfigRootDir: __dirname,
   },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      // use <root>/tsconfig.json
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root/>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'react-app',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+      },
+    },
+  ],
 };
